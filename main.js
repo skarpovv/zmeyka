07e2f92d9b190dc15cdb1d6bb64f91a0092b8c67
llfssaa <liubovitopolcean@gmail.com>
2022-10-22 00:09:56 +0300

    add tsconfig

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-var MyClass = /** @class */ (function () {
-    function MyClass() {
+var Snake = /** @class */ (function () {
+    function Snake(x, y) {
+        this.x = x;
+        this.y = y;
     }
-    return MyClass;
+    return Snake;
 }());
+var elem = document.getElementById('snake');
+setTimeout(function () {
+    elem.style.left = '30px';
+}, 2000);
+console.log();
--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-class MyClass {
-    private Hello: string;
+class Snake {
+    constructor (x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    x: number;
+    y: number;
+    color: string;
 }
+
+let elem = document.getElementById('snake')
+
+setTimeout(()=>{
+   elem!.style.left = '30px'
+}, 2000)
+
+console.log()
